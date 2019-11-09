6bfb913798ec93f4bbea3772fbfd22d2050fad11
Zach Lysobey <zlysobey@gmail.com>
2019-11-09 00:31:58 -0500

    ✨ add onChange handler

--- src/components/TypeaheadZ.tsx
+++ src/components/TypeaheadZ.tsx
@@ -6,7 +6,11 @@ export const TypeaheadZ: React.FC<{}> = () => {
     return (
         <form>
             <h1>Typeahead-Z!</h1>
-            <input type="text" placeholder="cool stuff here soon..." />
+            <input
+                type="text"
+                placeholder="cool stuff here soon..."
+                onChange={e => console.log('onChange', e.target.value)}
+            />
             <ul>
                 {typeaheadData.map(item => (
                     <li key={item}>{item}</li>
