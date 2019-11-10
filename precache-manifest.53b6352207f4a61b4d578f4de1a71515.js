7554deebec7e65498da9306505ffb9ce62f0e857
zachlysobey <zach@lysobey.com>
2019-11-10 13:52:36 +0000

    Deploying to gh-pages from master 187f0d1127514acabfbde03735236ec6938392d2

---
+++ precache-manifest.53b6352207f4a61b4d578f4de1a71515.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9640e9fef1c2597ac0ca88b89012b5c",
+    "url": "/typeahead-z/index.html"
+  },
+  {
+    "revision": "bb99740f3c05f147c477",
+    "url": "/typeahead-z/static/css/main.032384cb.chunk.css"
+  },
+  {
+    "revision": "4eaa0307d273e6f4e6ae",
+    "url": "/typeahead-z/static/js/2.3deab250.chunk.js"
+  },
+  {
+    "revision": "bb99740f3c05f147c477",
+    "url": "/typeahead-z/static/js/main.0649e0dd.chunk.js"
+  },
+  {
+    "revision": "22ad181a50275ae629e7",
+    "url": "/typeahead-z/static/js/runtime-main.d52fe325.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/typeahead-z/precache-manifest.53b6352207f4a61b4d578f4de1a71515.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/typeahead-z/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
